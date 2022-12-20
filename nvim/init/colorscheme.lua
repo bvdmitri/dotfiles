0a7f9a3abacdf27c09999eac064bf1a0d0a71238
Bagaev Dmitry <bvdmitri@gmail.com>
2022-12-20 11:09:56 +0100

    update colorscheme

--- nvim/init/colorscheme.lua
+++ nvim/init/colorscheme.lua
@@ -1,15 +1,14 @@
 
 -- Enable 24-bit RGB color in the UI
--- I disable the colors for a reason
--- I simply like it better for the everforest theme
-vim.opt.termguicolors = false
+vim.opt.termguicolors = true
 
 -- Everforest theme configuration
--- vim.g.everforest_background = 'hard'
--- vim.g.everforest_better_performance = 1
--- vim.g.everforest_colors_override = {
---   bg0 = { '#141414', '243' },
--- }
+vim.g.everforest_background = 'hard'
+vim.g.everforest_better_performance = 1
+vim.g.everforest_colors_override = {
+  bg0 = { '#080808', '243' },
+}
+
 -- Grubvox theme configuration
 -- vim.g.gruvbox_contrast_dark = 'hard'
 

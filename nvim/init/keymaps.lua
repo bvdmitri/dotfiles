7b1bb5b6f1f72d1709c60bca192af23a5cdd4d37
Bagaev Dmitry <bvdmitri@gmail.com>
2023-02-09 13:46:16 +0100

    update

--- nvim/init/keymaps.lua
+++ nvim/init/keymaps.lua
@@ -30,6 +30,17 @@ vim.keymap.set('n', '<Leader>ut', '<CMD>TSUpdate<CR>', opt)
 -- Make a window full screen
 vim.keymap.set('n', '<C-w>b', '<CMD>vertical resize 1000<CR>z1000<CR>', opt) 
 
+-- Window navigation
+-- Requirese: https://stackoverflow.com/questions/60870113/mac-generating-%E2%88%86%CB%9A%C2%AC-characters-instead-of-executing-vscode-shortcuts-that-involve
+vim.keymap.set('n', '<M-h>', '<C-w>h', opt)
+vim.keymap.set('n', '<M-j>', '<C-w>j', opt)
+vim.keymap.set('n', '<M-k>', '<C-w>k', opt)
+vim.keymap.set('n', '<M-l>', '<C-w>l', opt)
+vim.keymap.set('t', '<M-h>', '<C-\\><C-n><C-w>h', opt)
+vim.keymap.set('t', '<M-j>', '<C-\\><C-n><C-w>j', opt)
+vim.keymap.set('t', '<M-k>', '<C-\\><C-n><C-w>k', opt)
+vim.keymap.set('t', '<M-l>', '<C-\\><C-n><C-w>l', opt)
+
 -- Exit terminal mode on <Esc>, use <C-V><Esc> to actually use <Esc>
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opt)
 vim.keymap.set('t', '<C-v><Esc>', '<Esc>', opt)

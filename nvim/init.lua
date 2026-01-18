39df0a073e38017b08ba9b519988e70bfa2c1640
bvdmitri <bvdmitri@gmail.com>
2026-01-18 22:42:52 +0100

    use which-key

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,3 @@
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = 'a'
@@ -25,21 +24,22 @@ vim.o.pumblend = 15
 local gh = function(x) return 'https://github.com/' .. x end
 
 vim.pack.add({
-  { src = gh('neovim/nvim-lspconfig') },
-  { src = gh('nvim-mini/mini.indentscope') },
-  { src = gh('nvim-mini/mini.icons') },
-  { src = gh('nvim-mini/mini.snippets') },
-  { src = gh('nvim-mini/mini.completion') },
-  { src = gh('nvim-mini/mini.statusline') },
-  { src = gh('nvim-mini/mini.files') },
-  { src = gh('nvim-mini/mini.pick') },
-  { src = gh('nvim-mini/mini.extra') },
-  { src = gh('nvim-treesitter/nvim-treesitter') },
-  { src = gh('sainnhe/sonokai'), name = "theme-sonokai" },
-  { src = gh('rebelot/kanagawa.nvim'), name = "theme-kanagawa" },
+    { src = gh('neovim/nvim-lspconfig') },
+    { src = gh('nvim-mini/mini.indentscope') },
+    { src = gh('nvim-mini/mini.icons') },
+    { src = gh('nvim-mini/mini.snippets') },
+    { src = gh('nvim-mini/mini.completion') },
+    { src = gh('nvim-mini/mini.statusline') },
+    { src = gh('nvim-mini/mini.files') },
+    { src = gh('nvim-mini/mini.pick') },
+    { src = gh('nvim-mini/mini.extra') },
+    { src = gh('nvim-treesitter/nvim-treesitter') },
+    { src = gh('folke/which-key.nvim') },
+    { src = gh('sainnhe/sonokai'),                name = "theme-sonokai" },
+    { src = gh('rebelot/kanagawa.nvim'),          name = "theme-kanagawa" },
 })
 
--- I Like sonokai overall, but they use ugly black type of 
+-- I Like sonokai overall, but they use ugly black type of
 -- WinSeparator, so I change it to light gray instead
 vim.g.sonokai_dim_inactive_windows = 1
 vim.cmd("colorscheme sonokai | hi WinSeparator guifg='NvimDarkGray4'")
@@ -63,7 +63,7 @@ require('nvim-treesitter').install({ 'rust', 'javascript', 'typescript', 'python
 require('nvim-treesitter').update()
 
 vim.diagnostic.config({
-    virtual_text = true 
+    virtual_text = true
 })
 
 vim.lsp.config('basedpyright', {
@@ -85,29 +85,36 @@ vim.lsp.enable('ruff')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '\\[', ':e $MYVIMRC<CR>')
-vim.keymap.set('n', '\\]', MiniPick.builtin.help)
-vim.keymap.set('n', '\\h', ':noh<CR>')
+local wk = require('which-key')
 
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-vim.keymap.set('n', '<A-k>', ':resize -4<CR>')
-vim.keymap.set('n', '<A-j>', ':resize +4<CR>')
-vim.keymap.set('n', '<A-h>', ':vertical resize -4<CR>')
-vim.keymap.set('n', '<A-l>', ':vertical resize +4<CR>')
+wk.setup({ preset = 'helix' })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+wk.add({ '\\', group = 'Utilities' })
+vim.keymap.set('n', '\\[', ':e $MYVIMRC<CR>', { desc = 'Change my Neovim config' })
+vim.keymap.set('n', '\\]', MiniPick.builtin.help, { desc = 'Search help' })
+vim.keymap.set('n', '\\h', ':noh<CR>', { desc = 'Clear search highlight' })
+vim.keymap.set('n', '\\?', function() wk.show({ global = false }) end, { desc = 'Show buffer local keymaps (which-key)' })
 
-vim.keymap.set('n', '<leader>w', MiniPick.builtin.files)
-vim.keymap.set('n', '<leader>e', MiniExtra.pickers.marks)
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to the left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to the bottom window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to the top window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to the right window' })
+vim.keymap.set('n', '<A-k>', ':resize -4<CR>', { desc = 'Make window smaller horizontally' })
+vim.keymap.set('n', '<A-j>', ':resize +4<CR>', { desc = 'Make window larger horizontally' })
+vim.keymap.set('n', '<A-h>', ':vertical resize -4<CR>', { desc = 'Make window smaller vertically' })
+vim.keymap.set('n', '<A-l>', ':vertical resize +4<CR>', { desc = 'Make window larger vertically' })
+
+vim.keymap.set('n', '<leader>e', MiniFiles.open, { desc = 'Explorer MiniFiles (resume)' })
+vim.keymap.set('n', '<leader>E', function() MiniFiles.open(nil, false) end, { desc = 'Explorer MiniFiles (cwd)' })
+
+wk.add({ '<leader>f', group = 'File/Find' })
+vim.keymap.set('n', '<leader>ff', MiniPick.builtin.files, { desc = 'Find files (MiniPick)' })
+vim.keymap.set('n', '<leader>fb', MiniPick.builtin.buffers, { desc = 'Find buffers (MiniPick)' })
+vim.keymap.set('n', '<leader>fr', MiniPick.builtin.resume, { desc = 'Resume (MiniPick)' })
+-- vim.keymap.set('n', '<leader>e', MiniExtra.pickers.marks)
 vim.keymap.set('n', '<leader>r', MiniPick.builtin.resume)
 vim.keymap.set('n', '<leader>t', MiniPick.builtin.grep_live)
 vim.keymap.set('n', '<leader>a', '<C-^>')
-vim.keymap.set('n', '<leader>-', MiniFiles.open)
-vim.keymap.set('n', '<leader>_', function() MiniFiles.open(nil, false) end)
 
 vim.keymap.set('n', '<leader>x', ':confirm quit<CR>')
 vim.keymap.set('n', '<leader>u', vim.lsp.buf.format)
@@ -115,9 +122,9 @@ vim.keymap.set('n', '<leader>sa', vim.lsp.buf.code_action)
 vim.keymap.set('n', '<leader>sr', vim.lsp.buf.rename)
 vim.keymap.set('n', '<leader>si', vim.lsp.buf.incoming_calls)
 vim.keymap.set('n', '<leader>so', vim.lsp.buf.outgoing_calls)
-vim.keymap.set('n', '<leader>fr', [[:Pick lsp scope='references'<CR>]])
-vim.keymap.set('n', '<leader>fi', [[:Pick lsp scope='implementation'<CR>]])
-vim.keymap.set('n', '<leader>fd', [[:Pick lsp scope='definition'<CR>]])
+-- vim.keymap.set('n', '<leader>fr', [[:Pick lsp scope='references'<CR>]])
+-- vim.keymap.set('n', '<leader>fi', [[:Pick lsp scope='implementation'<CR>]])
+-- vim.keymap.set('n', '<leader>fd', [[:Pick lsp scope='definition'<CR>]])
 vim.keymap.set('n', '<leader>ds', [[:Pick lsp scope='document_symbol'<CR>]])
 vim.keymap.set('n', '<leader>ws', [[:Pick lsp scope='workspace_symbol_livelass '<CR>]])
 vim.keymap.set('n', '<leader>dd', [[:Pick diagnostic scope='current'<CR>]])

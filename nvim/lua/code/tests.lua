7d5b16d112629535a37744642b7768f96769e003
bvdmitri <bvdmitri@gmail.com>
2026-01-31 21:53:06 +0100

    Better keymaps for Neotest

--- nvim/lua/code/tests.lua
+++ nvim/lua/code/tests.lua
@@ -24,12 +24,22 @@ Neotest.setup({
     unknown = "󰄰",
     watching = ""
   },
+  summary = {
+    open = "botright vsplit | vertical resize 40"
+  }
 })
 
 local keymap = require('keymap')
 
 Neotest.run.run_file = function() Neotest.run.run(vim.fn.expand('%')) end
 Neotest.run.debug = function() Neotest.run.run({ strategy = 'dap' }) end
+Neotest.run.debug_last = function()
+  Neotest.run.run({ Neotest.run.get_last_run(), strategy = 'dap' })
+end
+-- Neotest.run.run_last is not nice, since it also will re-run the debugging 
+Neotest.run.rerun_last = function()
+  Neotest.run.run({ Neotest.run.get_last_run() })
+end
 
 Neotest.watch.toggle_file = function() Neotest.watch.toggle(vim.fn.expand('%')) end
 
@@ -41,8 +51,9 @@ keymap.nmap('<leader>tt', Neotest.run.run, 'Run the nearest test')
 keymap.nmap('<leader>tx', Neotest.run.stop, 'Stop the testing process')
 keymap.nmap('<leader>tm', Neotest.summary.run_marked, 'Run marked tests')
 keymap.nmap('<leader>tf', Neotest.run.run_file, 'Run the current file')
-keymap.nmap('<leader>td', Neotest.run.debug, 'Run the nearest test')
-keymap.nmap('<leader>t.', Neotest.run.run_last, 'Re-run the last test')
+keymap.nmap('<leader>td', Neotest.run.debug, 'Debug the nearest test')
+keymap.nmap('<leader>tD', Neotest.run.debug_last, 'Debug the last test')
+keymap.nmap('<leader>t.', Neotest.run.rerun_last, 'Re-run the last test')
 keymap.nmap('<leader>tw', Neotest.watch.toggle, 'Watch the nearest test')
 keymap.nmap('<leader>tW', Neotest.watch.toggle_file, 'Watch the current_file')
 keymap.nmap('<leader>tJ', Neotest.jump.next_failed, 'Next failed test')

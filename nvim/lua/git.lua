e7c6b425259ec93cf9f974a6e73f2fff35c11f08
bvdmitri <bvdmitri@gmail.com>
2026-01-31 10:07:32 +0100

    Add mini.diff plugin for diff highlighting

--- nvim/lua/git.lua
+++ nvim/lua/git.lua
@@ -1,14 +1,18 @@
 vim.pack.add({
     { src = gh('nvim-mini/mini-git') },
+    { src = gh('nvim-mini/mini.diff') },
 })
 
 local MiniGit = require('mini.git')
+local MiniDiff = require('mini.diff')
 
-MiniGit.setup({})
+MiniGit.setup()
+MiniDiff.setup()
 
 local keymap = require('keymap')
 
 keymap.add_group('Version Control', '<leader>v')
+-- MiniGit related commands
 keymap.nmap('<leader>vd', '<CMD>Git diff<CR>', 'Git diff')
 keymap.nmap('<leader>vD', '<CMD>Git diff --staged<CR>', 'Git diff staged')
 keymap.nmap('<leader>vl', '<CMD>Git log<CR>', 'Git log')
@@ -20,3 +24,5 @@ keymap.nmap('<leader>vc', '<CMD>Git commit -v<CR>', 'Git commit')
 keymap.nmap('<leader>vC', '<CMD>Git commit -a -v<CR>', 'Git commit all')
 keymap.nmap('<leader>vp', '<CMD>Git push<CR>', 'Git push')
 keymap.nmap('<leader>v]', MiniGit.show_at_cursor, 'Show at cursor')
+-- MiniDiff related commands
+keymap.nmap('<leader>vv', MiniDiff.toggle_overlay, 'Toggle diff overlay')

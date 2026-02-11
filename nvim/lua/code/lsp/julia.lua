78343b20c855f3250b84d41043448f646a80ec83
bvdmitri <bvdmitri@gmail.com>
2026-02-11 21:43:38 +0100

    try regular julia lsp

--- nvim/lua/code/lsp/julia.lua
+++ nvim/lua/code/lsp/julia.lua
@@ -21,14 +21,12 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
 })
 
 -- Just a note for myself
--- julia -e 'using Pkg; Pkg.Apps.add(; url="https://github.com/aviatesk/JETLS.jl", rev="release")'
-vim.lsp.config("jetls", {
-    cmd = {
-        "jetls",
-        "--threads=auto",
-        "--",
-    },
-    filetypes = { "julia" },
-    root_markers = { "Project.toml" }
-})
-vim.lsp.enable("jetls")
+-- LanguageServer.jl, SymbolServer.jl and StaticLint.jl can be installed with `julia` and `Pkg` >sh
+--   julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer"); Pkg.add("SymbolServer"); Pkg.add("StaticLint")'
+-- where `~/.julia/environments/nvim-lspconfig` is the location where
+-- the default configuration expects LanguageServer.jl, SymbolServer.jl and StaticLint.jl to be installed.
+--
+-- To update an existing install, use the following command >sh
+--   julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.update()'
+--
+vim.lsp.enable("julials")

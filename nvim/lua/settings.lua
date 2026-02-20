312fbda8ce2f571f6c01a177646675bf871e9323
bvdmitri <bvdmitri@gmail.com>
2026-02-20 20:56:53 +0100

    docs(settings): add inline comments to configuration options

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,37 +1,37 @@
 local g, o = vim.g, vim.o
 
-g.mapleader = ' '
-g.maplocalleader = ' '
-g.winborder = "rounded"
+g.mapleader = ' '                     -- Set leader key to space
+g.maplocalleader = ' '                -- Set local leader key to space
+g.winborder = "rounded"               -- Default border style for windows
 
-o.number = true
-o.relativenumber = true
-o.shiftwidth = 4
-o.softtabstop = 2
-o.expandtab = true
-o.smarttab = true
-o.mouse = 'a'
-o.splitright = true
-o.splitbelow = true
-o.signcolumn = "yes"
-o.winwidth = 20
-o.winheight = 10
-o.winborder = "rounded"
-o.winblend = 5
-o.pumborder = "rounded"
-o.pumheight = 10
-o.pummaxwidth = 45
-o.pumblend = 5
-o.colorcolumn = '80'
-o.list = true
-o.listchars = "tab:» ,trail:·,nbsp:␣"
-o.inccommand = 'split'
-o.updatetime = 250
-o.cursorline = true
-o.scrolloff = 10
-o.ignorecase = true
-o.smartcase = true
-o.breakindent = true
+o.number = true                       -- Show absolute line numbers
+o.relativenumber = true               -- Show relative line numbers
+o.shiftwidth = 4                      -- Number of spaces for autoindent
+o.softtabstop = 2                     -- Number of spaces for tabstop
+o.expandtab = true                    -- Convert tabs to spaces
+o.smarttab = true                     -- Tab respects shiftwidth at line start
+o.mouse = 'a'                         -- Enable mouse in all modes
+o.splitright = true                   -- Vertical splits open to the right
+o.splitbelow = true                   -- Horizontal splits open below
+o.signcolumn = "yes"                  -- Always show sign column
+o.winwidth = 20                       -- Minimum width for active window
+o.winheight = 10                      -- Minimum height for active window
+o.winborder = "rounded"               -- Border style for windows
+o.winblend = 5                        -- Transparency for windows (0-100)
+o.pumborder = "rounded"               -- Border style for popup menu
+o.pumheight = 10                      -- Maximum height for popup menu
+o.pummaxwidth = 45                    -- Maximum width for popup menu
+o.pumblend = 5                        -- Transparency for popup menu (0-100)
+o.colorcolumn = '80'                  -- Highlight column at 80 characters
+o.list = true                         -- Show hidden characters
+o.listchars = "tab:» ,trail:·,nbsp:␣" -- Characters for hidden whitespace
+o.inccommand = 'split'                -- Show substitute preview in split
+o.updatetime = 250                    -- Time before CursorHold event (ms)
+o.cursorline = true                   -- Highlight current line
+o.scrolloff = 10                      -- Minimum lines above/below cursor
+o.ignorecase = true                   -- Ignore case in search patterns
+o.smartcase = true                    -- Override ignorecase with uppercase
+o.breakindent = true                  -- Wrapped lines match indent
 
 vim.diagnostic.config({
     severity_sort = true,
@@ -52,7 +52,6 @@ vim.diagnostic.config({
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-    callback = function()
-        vim.hl.on_yank()
-    end,
+    callback = function() vim.hl.on_yank() end,
 })
+

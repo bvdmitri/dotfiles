e74c3e387902dff4cfda5604572487c114fcddf8
bvdmitri <bvdmitri@gmail.com>
2026-03-05 10:35:22 +0100

    enable code formatter for typst documents

--- nvim/lua/code/lsp/typst.lua
+++ nvim/lua/code/lsp/typst.lua
@@ -2,6 +2,12 @@ vim.pack.add({
     { src = gh('chomosuke/typst-preview.nvim') }
 })
 
+vim.lsp.config['tinymist'] = {
+    settings = {
+        formatterMode = "typstyle"
+    }
+}
+
 vim.lsp.enable('tinymist')
 
 require('typst-preview').setup()

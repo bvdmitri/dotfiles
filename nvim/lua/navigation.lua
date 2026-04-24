3b7b8226ea267aa6287e08106387466f040fa261
Bagaev Dmitry <bvdmitri@gmail.com>
2026-04-24 12:08:42 +0200

    update

--- nvim/lua/navigation.lua
+++ nvim/lua/navigation.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
     { src = gh('nvim-mini/mini.jump2d') },
-    { src = gh('bvdmitri/recall.nvim'), version = 'support-for-tab-pages' },
+    { src = gh('fnune/recall.nvim') },
 })
 
 local keymap = require('keymap')
